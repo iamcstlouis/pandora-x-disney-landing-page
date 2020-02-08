6bbdd41240b26a47ff4458cc38718bfec8d0b9f9
Chantal St Louis <misscstlouis@gmail.com>
2020-02-08 23:59:01 +0000

    Scroll reveal added

--- src/main.js
+++ src/main.js
@@ -3,3 +3,20 @@ import '@/assets/css/app.scss'
 
 // Import bootstrap js
 import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+
+// ----------------------------------------------------------------
+//                         Scroll Reveal
+// ----------------------------------------------------------------
+
+ScrollReveal().reveal('.fast-reveal', {
+    delay: 500
+});
+
+ScrollReveal().reveal('.mid-reveal', {
+    delay: 900
+});
+
+ScrollReveal().reveal('.slow-reveal', {
+    delay: 1400
+});
